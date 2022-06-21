782ca9b64963c2da83466db2725791b36a1fd0ae
kamkow1 <ikamileq@gmail.com>
2022-06-22 00:54:29 +0200

    add generation script

---
+++ src/spli/scripts/generate.sh
@@ -0,0 +1,17 @@
+action=$1
+
+jar_loc=""
+
+if [[ -z "$2" ]]; then
+  jar_loc="/usr/local/bin/antlr-4.10.1-complete.jar"
+else
+  jar_loc=$2
+fi
+
+if [ "$action" = "lexer" ]; then
+  eval "java -jar $jar_loc ./AntlrDefinitions/SpringLexer.g4 -Dlanguage=CSharp -o ./Lexer"
+elif [ "$action" = "parser" ]; then
+  eval "java -jar $jar_loc ./AntlrDefinitions/SpringParser.g4 -Dlanguage=CSharp -o ./Parser"
+else
+  echo "action not supported"
+fi
