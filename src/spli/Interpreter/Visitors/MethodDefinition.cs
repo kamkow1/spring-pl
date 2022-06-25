9b3e927c92f722354092a47de8e2732562e7d5d7
kamkow1 <ikamileq@gmail.com>
2022-06-25 17:28:34 +0200

    decouple visitor - method def

---
+++ src/spli/Interpreter/Visitors/MethodDefinition.cs
@@ -0,0 +1,28 @@
+using Antlr4.Runtime.Misc;
+
+namespace spli.Interpreter.Visitors;
+
+public partial class Visitor
+{
+    public override object VisitMethod_def([NotNull] SpringParser.Method_defContext context)
+    {
+        var name = context.IDENTIFIER(0).GetText();
+
+        if (_builtinFunctions.ContainsKey(name))
+            throw new Exception($"function {name} already exists");
+
+        var parameters = new List<string>();
+
+        for(var i = 1; i < context.IDENTIFIER().Length; ++i) 
+        {
+            parameters.Add(context.IDENTIFIER(i).GetText());
+        }
+
+        var statements = context.scope().statement();
+
+        var accessModifier = context.access_mod().GetText();
+        var isPublic = accessModifier == "pub";
+
+        return new Method(name, isPublic, statements, parameters.ToArray());
+    }
+}
--- src/spli/Interpreter/Visitors/Visitor.cs
+++ src/spli/Interpreter/Visitors/Visitor.cs
@@ -79,27 +79,7 @@ public partial class Visitor : SpringParserBaseVisitor<Object?>
 
     
 
-    public override object VisitMethod_def([NotNull] SpringParser.Method_defContext context)
-    {
-        var name = context.IDENTIFIER(0).GetText();
-
-        if (_builtinFunctions.ContainsKey(name))
-            throw new Exception($"function {name} already exists");
-
-        var parameters = new List<string>();
-
-        for(var i = 1; i < context.IDENTIFIER().Length; ++i) 
-        {
-            parameters.Add(context.IDENTIFIER(i).GetText());
-        }
-
-        var statements = context.scope().statement();
-
-        var accessModifier = context.access_mod().GetText();
-        var isPublic = accessModifier == "pub";
-
-        return new Method(name, isPublic, statements, parameters.ToArray());
-    }
+    
 
     public override object? VisitPropAccessExpression([NotNull] SpringParser.PropAccessExpressionContext context)
     {
