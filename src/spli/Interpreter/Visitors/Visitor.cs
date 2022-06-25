0a6f94581d19cb90a6e5c2a949416ba52f7d7c0e
kamkow1 <ikamileq@gmail.com>
2022-06-25 17:23:59 +0200

    decouple visitor - each loop statement

--- src/spli/Interpreter/Visitors/Visitor.cs
+++ src/spli/Interpreter/Visitors/Visitor.cs
@@ -71,78 +71,7 @@ public partial class Visitor : SpringParserBaseVisitor<Object?>
 
     
 
-    public override object? VisitEach_loop_statement([NotNull] SpringParser.Each_loop_statementContext context)
-    {
-        var config = (EachConfiguration)Visit(context.expression())!;
-
-        if (config.OptionalIteratorName is not null)
-        {
-            foreach(var item in config.Array.Select((value, i) => new { value, i }))
-            {
-                if (_shouldExitCurrentLoop)
-                {
-                    _shouldExitCurrentLoop = false;
-                    break;
-                }
-
-                var activationRecord = new ActivationRecord();
-
-                activationRecord.SetItem(config.ItemName, item.value);
-                activationRecord.SetItem(config.OptionalIteratorName, item.i);
-
-                _stack.Push(activationRecord);
-                foreach (var statement in context.scope().statement())
-                {
-                    if (statement.return_statement() is {} returnStatement)
-                        return Visit(returnStatement.expression());
-
-                    if (_shouldSkipCurrentIteration)
-                    {
-                        _shouldSkipCurrentIteration = false;
-                        continue;
-                    }
-
-                    Visit(statement);
-                }
-                _stack.Pop();
-            }
-        }
-        else
-        {
-            foreach(var item in config.Array)
-            {
-                if (_shouldExitCurrentLoop)
-                {
-                    _shouldExitCurrentLoop = false;
-                    break;
-                }
-
-                var activationRecord = new ActivationRecord();
-
-                activationRecord.SetItem(config.ItemName, item);
-
-                _stack.Push(activationRecord);
-                foreach (var statement in context.scope().statement())
-                {
-
-                    if (_shouldSkipCurrentIteration)
-                    {
-                        _shouldSkipCurrentIteration = false;
-                        continue;
-                    }
-
-                    if (statement.return_statement() is {} returnStatement)
-                        return Visit(returnStatement.expression());
-
-
-                    Visit(statement);
-                }
-                _stack.Pop();
-            }
-        }
-
-        return null;
-    }
+    
 
     public override object VisitEachLoopExpression([NotNull] SpringParser.EachLoopExpressionContext context)
     {
