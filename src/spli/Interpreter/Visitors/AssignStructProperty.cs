5674da47fc0b1d2aa2932a9dd8f1435ce59e89a4
kamkow1 <ikamileq@gmail.com>
2022-06-25 17:32:23 +0200

    decouple visitor - assign struct prop

---
+++ src/spli/Interpreter/Visitors/AssignStructProperty.cs
@@ -0,0 +1,26 @@
+using Antlr4.Runtime.Misc;
+
+namespace spli.Interpreter.Visitors;
+
+public partial class Visitor
+{
+    public override object VisitAssign_struct_prop([NotNull] SpringParser.Assign_struct_propContext context)
+    {
+        var value = Visit(context.expression(0));
+        var structure = (StructureInstance)Visit(context.expression(1))!;
+
+        var propName = context.IDENTIFIER().GetText();
+
+        if (structure.Name == "self")
+        {
+            structure.BaseStructure.Props[propName].Value = value;
+            return true;
+        }
+
+        if (structure.BaseStructure.Props[propName].IsPublic)
+            structure.BaseStructure.Props[propName].Value = value;
+        else throw new Exception($"cannot access a private property {propName}");
+
+        return true;
+    }
+}
--- src/spli/Interpreter/Visitors/Visitor.cs
+++ src/spli/Interpreter/Visitors/Visitor.cs
@@ -85,25 +85,7 @@ public partial class Visitor : SpringParserBaseVisitor<Object?>
 
     
 
-    public override object VisitAssign_struct_prop([NotNull] SpringParser.Assign_struct_propContext context)
-    {
-        var value = Visit(context.expression(0));
-        var structure = (StructureInstance)Visit(context.expression(1))!;
-
-        var propName = context.IDENTIFIER().GetText();
-
-        if (structure.Name == "self")
-        {
-            structure.BaseStructure.Props[propName].Value = value;
-            return true;
-        }
-
-        if (structure.BaseStructure.Props[propName].IsPublic)
-            structure.BaseStructure.Props[propName].Value = value;
-        else throw new Exception($"cannot access a private property {propName}");
-
-        return true;
-    }
+    
 
     public override object? VisitMethodCallExpression([NotNull] SpringParser.MethodCallExpressionContext context)
     {
